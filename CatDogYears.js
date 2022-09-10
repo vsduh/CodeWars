8cf84668d89da86fb6cc886954eae0e93250f337
VyacheslavDukhanin <ya@live.ru>
2022-09-10 20:47:34 +0300

    ready

--- CatDogYears.js
+++ CatDogYears.js
@@ -1,33 +1,26 @@
 
-describe("Example Tests", function() {
 
-    it("one", function() {
-        Test.assertSimilar(humanYearsCatYearsDogYears(1), [1,15,15]);
-    });
-
-    it("two", function() {
-        Test.assertSimilar(humanYearsCatYearsDogYears(2), [2,24,24]);
-    });
-
-    it("ten", function() {
-        Test.assertSimilar(humanYearsCatYearsDogYears(10), [10,56,64]);
-    });
-
-});
 
 var humanYearsCatYearsDogYears = function(humanYears) {
     let catYears = 0
     let dogYears = 0
-    let k = humanYears;
-    while (k != 2) { dogYears += 5, catYears += 4, k--
+
+    for (let k = humanYears;k != 2;k--) { dogYears += 5, catYears += 4};
+    if (k == 2) {
+        dogYears = dogYears + 9;
+        catYears = catYears + 9;
+
+        dogYears = dogYears + 15;
+        catYears = catYears + 15;
+
+
+        return [humanYears, catYears, dogYears];
     }
-    if (k = 2) {
-        dogYears = dogYears + 9; catYears = catYears + 9;
-    }
-    dogYears = dogYears + 15; catYears = catYears + 15;
 }
 
-return [humanYears, catYears, dogYears];
 
-}
+
+
+
+
 
